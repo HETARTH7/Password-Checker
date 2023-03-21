2febc0c1502ce50660c15e92a25328e8deba0223
unknown <hetarth002@gmail.com>
2023-03-21 23:54:01 +0530

    create flask app

---
+++ server.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+import hashlib
+import requests
+
+app = Flask(__name__)
+
+def request_api_data(char):
+    url = 'https://api.pwnedpasswords.com/range/'+char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(
+            f'Error fetching: {res.status_code}, check the api and try again')
+    return res
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = None
+    if request.method == 'POST':
+        password = request.form['password']
+        count = pwned_api_check(password)
+        if count:
+            result = f'{password} was found {count} times. You should change it'
+        else:
+            result = f"{password} wasn't found. You're good to go"
+    return render_template('index.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
